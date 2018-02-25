8e42b82316ab977a229dd4d2ebc08c63ca0d389e
Atsushi Sasaki <atsaki01@gmail.com>
2018-02-26 07:07:18 +0900

    Bump version to v0.1.5

--- docker-machine-driver-cloudstack.rb
+++ docker-machine-driver-cloudstack.rb
@@ -3,15 +3,14 @@ require "language/go"
 class DockerMachineDriverCloudstack < Formula
   desc "Docker Machine generic CloudStack driver"
   homepage "https://github.com/atsaki/docker-machine-driver-cloudstack"
-  url "https://github.com/atsaki/docker-machine-driver-cloudstack/archive/v0.1.4.tar.gz"
-  version "v0.1.4"
-  sha256 "f956a1df446aebe2edeb41033fadb97e332b61099e650a84c693ed737a1d9515"
+  url "https://github.com/atsaki/docker-machine-driver-cloudstack/archive/v0.1.5.tar.gz"
+  version "v0.1.5"
+  sha256 "04b77ab5e1d9ed865a3ee2ad8ba4a303a54eab52f94bd4ebf3c21697ba5311aa"
   head "https://github.com/atsaki/docker-machine-driver-cloudstack.git"
 
   bottle do
-    root_url "https://bintray.com/artifact/download/atsaki/bottles"
     cellar :any_skip_relocation
-    sha256 "9191224b8fa0894e167f5391dd245cc4bd95871372c1e7f130481dfec01cb095" => :el_capitan
+    sha256 "fa10d26f5e9f8d51ea231e21c7216d49d220db3dcb5112874f05b3d887474a9e" => :el_capitan
   end
 
   depends_on "go" => :build
@@ -23,17 +22,17 @@ class DockerMachineDriverCloudstack < Formula
 
   go_resource "github.com/docker/machine" do
     url "https://github.com/docker/machine.git",
-      :revision => "a650a404fc3e006fea17b12615266168db79c776"
+      :revision => "0c3f538eb470d1546c42f82646000726e237c2b6"
   end
 
   go_resource "golang.org/x/crypto" do
     url "https://go.googlesource.com/crypto.git",
-      :revision => "beef0f4390813b96e8e68fd78570396d0f4751fc"
+      :revision => "51714a8c4ac1764f07ab4127d7f739351ced4759"
   end
 
   go_resource "github.com/xanzy/go-cloudstack" do
     url "https://github.com/xanzy/go-cloudstack.git",
-      :revision => "104168fa792713f5e04b76e2862779dc2ad85bcc"
+      :revision => "c815114af88405d9a8464cab25b71af3b38547c9"
   end
 
   def install
